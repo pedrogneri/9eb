0956b94a235f6371dbc6ca9262250ac25c895544
Pedro Neri <pedrogneri@gmail.com>
2022-04-19 20:51:01 -0300

    feat: altera função de getRandomWord para utilizar whitelist

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -1,6 +1,8 @@
 import { WORDS } from "./entries"
+import { WHITE_LIST } from "./whitelist";
 
 const wordsArray = WORDS.split(',');
+const whiteListArray = WHITE_LIST.split(',');
 
 const getRandomIndex = (min: number, max: number) => Math.trunc(Math.random() * (max - min) + min);
 
@@ -8,4 +10,12 @@ export const findWord = (value: string) => wordsArray.find(v => normalizeWord(v)
 
 export const normalizeWord = (value: string) => value.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
 
-export const getRandomWord = () => wordsArray[getRandomIndex(0, wordsArray.length - 1)];
+export const getRandomWord = (): string => {
+  const randomWord = whiteListArray[getRandomIndex(0, whiteListArray.length - 1)];
+
+  if (wordsArray.find((v) => v === randomWord)) {
+    return randomWord;
+  } 
+
+  return getRandomWord();
+}
