8c4525ee3d08174097df84817bf32af597e472f0
Pedro Neri <pedrogneri@gmail.com>
2022-04-19 16:31:16 -0300

    feat: altera keydown para view

--- src/components/row/row.tsx
+++ src/components/row/row.tsx
@@ -1,41 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Letter } from '../letter';
 import * as S from './row.style';
 
 type Props = {
+  value: string;
   isSelected: boolean;
 }
 
-const Row = ({ isSelected }: Props) => {
-  const [value, setValue] = useState('');
-
-  useEffect(() => {
-    const onKeyDown = (e: KeyboardEvent) => {
-      const { key } = e;
-      const isCharKey = key.match("[a-zA-Z]*\\b") && key.length === 1;
-
-      if (!isSelected) return;
-
-      if (isCharKey && value.length < 5) {
-        setValue(v => v + key)
-      }
-
-      if (key === 'Backspace') {
-        setValue(v => v.slice(0, -1));
-      }
-    }
-
-    document.addEventListener('keydown', onKeyDown);
-
-    return (): void => {
-      document.removeEventListener('keydown', onKeyDown);
-    };
-  }, [value.length, isSelected])
-
+const Row = ({ value, isSelected }: Props) => {
   return (
     <S.Container>
       {[...Array(5)].map((_, index) => (
-        <Letter 
+        <Letter
           key={index.toString()}
           value={value[index] || ''} 
           isSelected={isSelected} 
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,13 +1,67 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Row } from '../../components';
 import * as S from './home.style';
 
+const ROWS = 6;
+
 const Home = () => {
+  const [rowIndex, setRowIndex] = useState(0);
+  const [value, setValue] = useState('');
+
+  const [tries, setTries] = useState(Array(ROWS).fill(''));
+
+  const changeTry = useCallback((newValue: string) => {
+    setTries(v => {
+      const newTries = [...v];
+      newTries[rowIndex] = newValue;
+
+      return newTries;
+    })
+  }, [rowIndex])
+
+  useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      const { key } = e;
+      const isCharKey = key.match("[a-zA-Z]*\\b") && key.length === 1;
+
+      if (isCharKey && value.length < 5) {
+        setValue(v => {
+          const newValue = v + key;
+          changeTry(newValue);
+          return newValue;
+        })
+      }
+
+      if (key === 'Backspace') {
+        setValue(v => {
+          const newValue = v.slice(0, -1);
+          changeTry(newValue);
+          return newValue;
+        })
+      }
+
+      if (key === 'Enter' && rowIndex < ROWS - 1) {
+        setValue('');
+        setRowIndex(v => v + 1)
+      }
+    }
+
+    document.addEventListener('keydown', onKeyDown);
+
+    return (): void => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, [value.length, rowIndex, value, changeTry])
+
   return (
     <S.Container>
       <S.Board>
-        {[...Array(6)].map((_, index) => (
-          <Row key={index.toString()} />
+        {[...Array(ROWS)].map((_, index) => (
+          <Row
+            key={index.toString()}
+            value={tries[index]}
+            isSelected={index === rowIndex}
+          />
         ))}
       </S.Board>
     </S.Container>
