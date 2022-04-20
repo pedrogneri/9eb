7c1fce70517d731310769d16167e50cdd2aafab9
Pedro Neri <pedrogneri@gmail.com>
2022-04-20 11:50:32 -0300

    fix: ajusta state de contain da letter

--- src/components/row/row.tsx
+++ src/components/row/row.tsx
@@ -6,24 +6,50 @@ import * as S from './row.style';
 
 type Props = {
   word: string;
-  value: string;
+  input: string;
   isSelected: boolean;
 }
 
-const Row = ({ word, value, isSelected }: Props) => {
+const Row = ({ word, input, isSelected }: Props) => {
   const getLetterState = (index: number): State => {
-    const normalizedWord = normalizeWord(word);
-    const normalizedValue = normalizeWord(value);
+    const wordLetters = normalizeWord(word).split('');
+    const inputLetters = normalizeWord(input).split('');
+
+    const wordChar = wordLetters[index];
+    const inputChar = inputLetters[index];
 
     if (isSelected) {
       return 'default'
     }
 
-    if (normalizedValue[index] === normalizedWord[index]) {
+    if (wordChar === inputChar) {
       return 'correct';
     }
 
-    if (normalizedWord.includes(normalizedValue[index])) {
+    if (wordLetters.includes(inputChar)) {
+      let previousRepeat = 0;
+
+      const inputMatches = inputLetters.filter((v, i) => {
+        if (v === inputChar && i < index) {
+          previousRepeat += 1;
+        }
+        return v === inputChar
+      });
+
+      if(inputMatches.length > 1) {
+        const correctPositions = wordLetters.filter(
+          (v, i) => v === inputLetters[i] && v === inputChar
+        );
+        const wordMatches = wordLetters.filter((v) => v === inputChar);
+
+        if (
+          (correctPositions.length < inputMatches.length && inputMatches.length < wordMatches.length) ||
+          (previousRepeat >= wordMatches.length)
+        ) {
+          return 'default';
+        }
+      }
+
       return 'contain';
     }
 
@@ -35,7 +61,7 @@ const Row = ({ word, value, isSelected }: Props) => {
       {[...Array(5)].map((_, index) => (
         <Letter
           key={index.toString()}
-          value={value[index] || ''} 
+          value={input[index] || ''} 
           isSelected={isSelected}
           state={getLetterState(index)}
         />
